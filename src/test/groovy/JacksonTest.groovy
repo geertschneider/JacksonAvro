60daf3dda0a85147f708280a1160abfb9986755c
Geert Schneider <geert.schneider.be@gmail.com>
2020-11-19 12:56:10 +0100

    working test but very slow

--- src/test/groovy/JacksonTest.groovy
+++ src/test/groovy/JacksonTest.groovy
@@ -1,4 +1,19 @@
+import be.vdab.vdp.JacksonAvroSerDe
+import com.fasterxml.jackson.dataformat.avro.AvroMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import groovy.transform.CompileStatic
+import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig
 import kafka.Kafka
+import org.apache.kafka.clients.consumer.Consumer
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.clients.consumer.ConsumerRecords
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.Producer
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.serialization.Serdes
+import org.apache.kafka.common.serialization.StringSerializer
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.testcontainers.containers.DockerComposeContainer
@@ -11,12 +26,17 @@ import org.testcontainers.junit.jupiter.Testcontainers
 import org.testcontainers.lifecycle.Startable
 import org.testcontainers.utility.DockerImageName
 
+import java.time.Duration
+
 @Testcontainers
 class JacksonTest {
     static final String registry = 'schema-registry'
+    static final Integer registryPort  = 8081
+
     static final String kafka = 'kafka'
     static final Integer kafkaPort  = 9092
-    static final Integer registryPort  = 8081
+
+    String kafkaURL,schemaRegistryURL
 
     @Container
     DockerComposeContainer containers =
@@ -24,57 +44,107 @@ class JacksonTest {
                     .withExposedService( registry,registryPort)
                     .withExposedService(kafka,kafkaPort)
 
-//    @Container
-//    GenericContainer zookeeperCont=new GenericContainer(DockerImageName.parse('confluentinc/cp-zookeeper:5.5.0'))
-
-//    @Container
-//    GenericContainer kafkaCont=new GenericContainer(DockerImageName.parse('confluentinc/cp-zookeeper:5.5.0'))
-//            .withEnv(
-//                    [
-//                            KAFKA_ZOOKEEPER_CONNECT:'zookeeper:2181',
-//                            KAFKA_LISTENER_SECURITY_PROTOCOL_MAP: 'PLAINTEXT:PLAINTEXT,PLAINTEXT_HOST:PLAINTEXT',
-//                            KAFKA_INTER_BROKER_LISTENER_NAME: 'PLAINTEXT',
-//                            KAFKA_ADVERTISED_LISTENERS: 'PLAINTEXT://kafka:29092,PLAINTEXT_HOST://localhost:9092',
-//                            KAFKA_AUTO_CREATE_TOPICS_ENABLE:'true'
-//                    ]
-//            ).withExposedPorts(9092)
-//            .dependsOn([zookeeperCont])
-//    @Container
-//    KafkaContainer kafkaCont=new KafkaContainer(DockerImageName.parse('confluentinc/cp-kafka:5.5.0')).withExternalZookeeper('zookeeper:2181').dependsOn([zookeeperCont])
-////
-//    @Container
-//    GenericContainer schemaRegistryCont=new GenericContainer(DockerImageName.parse('confluentinc/cp-zookeeper:5.5.0'))
-//            .withEnv(
-//                    [
-//                            SCHEMA_REGISTRY_KAFKASTORE_CONNECTION_URL: 'zookeeper:2181'
-//                    ]
-//            )
-//            .dependsOn([zookeeperCont,kafkaCont])
-
-
 
     @Test
-    void CheckKafka(){
-println('containers started')
+    void CheckSerialization(){
+
+
+        def kafkaCont = [
+                host : containers.getServiceHost(kafka,kafkaPort),
+                port : containers.getServicePort(kafka,kafkaPort)
+        ]
+        kafkaURL="${kafkaCont.host}:${kafkaCont.port}"
+
+        def registryCont = [
+                host : containers.getServiceHost(registry,registryPort),
+                port : containers.getServicePort(registry,registryPort)
+        ]
+
+        schemaRegistryURL="http://${registryCont.host}:${registryCont.port}"
+
+        runProducer()
+
+//        runConsumer()
+
+        Assertions.assertTrue(true)
 
     }
 
-//    @Test
-//    void CheckKafkaRuns(){
-//
-//
-//        containers.waitingFor(registry,Wait.forHttp().forPort(registryPort))
-//        containers.waitingFor(kafka,Wait.forHealthcheck())
-//
-//
-////
-////        Integer schemaPort = containers.getServicePort(registry,8081)
-////        Integer kafkaPort = containers.getServicePort('kafka',9092)
-////
-////        containers.waitingFor('schema-registry', Wait.forHttp(schemaPort) )
-//        String host= containers.getServiceHost(kafka,kafkaPort)
-//        def port= containers.getServicePort(kafka,kafkaPort)
-//        println "kafka  running at ${host} - ${port}"
-//        Assertions.assertTrue(true)
-//    }
+
+
+    def runProducer(){
+        println 'running producer'
+        Producer<String, User> producer = createProducer()
+        (1..10).each({
+            User usr = new User(id: it, naam: "Geert-${it}", vacatures: [new Vacature(id: it)])
+            producer.send(new ProducerRecord<String, User>('testtopic', 'A', usr))
+            println "produced event ${it}"
+        })
+        producer.flush()
+        producer.close()
+    }
+    def runConsumer(){
+        println 'running consumer'
+        Consumer<String, User> consumer = createConsumer()
+        Integer amountOfRecords=0
+
+        (1..5).each{
+
+            ConsumerRecords<String,User> consumedRecords= consumer.poll( Duration.ofSeconds(1,0))
+            consumedRecords.each {consumerRecord ->
+                println consumerRecord.value()
+                amountOfRecords++
+            }
+
+
+            if(amountOfRecords==10)
+                return true
+        }
+        return false
+    }
+
+
+
+    Producer<String, User> createProducer() {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaURL)
+        props.put(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryURL)
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, 'testClient');
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        AvroMapper mapper = new AvroMapper()
+        mapper.registerModule(new JavaTimeModule())
+        JacksonAvroSerDe<User> jacksonSerDe= new JacksonAvroSerDe<User>(User.class, mapper)
+
+        HashMap<String,String> config= new HashMap<String,String>()
+        config.put(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryURL)
+        jacksonSerDe.configure( config  ,false)
+
+        return new KafkaProducer<>(props,Serdes.String().serializer(),jacksonSerDe.serializer())
+//        return new KafkaProducer<String, String>(props)
+    }
+
+
+    Consumer<String, User> createConsumer() {
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaURL)
+        props.put(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryURL)
+        props.put(ConsumerConfig.CLIENT_ID_CONFIG, 'testClient');
+        props.put(ConsumerConfig.GROUP_ID_CONFIG,'testGroup')
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        AvroMapper mapper = new AvroMapper()
+        mapper.registerModule(new JavaTimeModule())
+        JacksonAvroSerDe<User> jacksonSerDe= new JacksonAvroSerDe<User>(User.class, mapper)
+
+        HashMap<String,String> config= new HashMap<String,String>()
+        config.put(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryURL)
+        jacksonSerDe.configure( config  ,false)
+
+        KafkaConsumer<String,User> consumer= new KafkaConsumer<String,User>(props, Serdes.String().deserializer(),jacksonSerDe.deserializer())
+        consumer.subscribe(['testtopic'])
+        return consumer
+    }
 }
